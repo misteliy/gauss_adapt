1ecda76b34dac44e3213f58ca8be8ca094d910bc
cp2k <yannickmisteli@gmail.com>
2012-01-17 10:15:43 +0100

    working now

--- test_gauss.f90
+++ test_gauss.f90
@@ -3,8 +3,8 @@
     use random
 	implicit none
 
-	integer,parameter                  :: n=10,maxiter=10000 
-	character(60)                      :: task
+	integer,parameter                  :: n=30,maxiter=1e5
+	character(60)                      :: task,fmt,ci
 	real(kind=dp)                      :: f,c_t,x(n),eta(n),m(n),eps
 	real(kind=dp)                      :: Q(n,n),C(n,n),r,xmin(n),fmin,detQ
 	integer                            :: i,counter(2),indx(n)
@@ -18,8 +18,8 @@
     call random_number(x)
     x=(x-0.5_dp)*10._dp
 	counter=0
-	iprint = .true.
-	
+	iprint = .true. 
+!    iprint = .false.	
 	if (iprint) print *,'**********************start************************'
 	do i = 0,maxiter
 		if (task(1:5).eq.'new_x') call fun(x,n,f)
@@ -33,7 +33,9 @@
 		if (iprint) then
 			print *,'---------------------------------'
 			print *,'loop',i
-			write(*,'(A,1X,10E13.6E1)') 'x',x
+            write(ci,*) n
+            fmt='(A,1X,' // trim(ci) //'E13.6E1)'
+			write(*,fmt) 'x',x
             print *,'f',f,'ct',c_t
 !			print *,'m',m
 !			print *,'xmin',xmin,'fmin',fmin
@@ -64,11 +66,55 @@
 	use random
 	implicit none
 	integer,intent(in)   :: n
-	real(kind=dp) :: x(n),a,f
-    f = sum(x**2)
-!	f = (x(1)-5._dp)**2 + (x(2)-3._dp)**2
-	!a=sqrt(2._dp)
-	!f = 100*(x(2)-x(1)**2)**2+(a-x(1))
+    integer              :: i,j,funct
+    real(kind=dp),intent(out) :: f
+	real(kind=dp) :: x(n),a,temp,rnd
+    real(kind=dp) :: z(n),M(n,n)
+    real(kind=dp),parameter :: pi=2*asin(1._dp)
+
+    funct=1
+    f=0._dp
+    select case(funct)
+!--------------------------------de jongs function---------------------
+    case(1)
+        f = sum(x**2)
+!--------------------------------schwefels problem---------------------
+    case(2)
+        do i=1,n
+            temp=0._dp
+            do j=1,i
+               temp=temp+x(j)
+            enddo
+            f = f + temp**2
+        enddo            
+!-------------------------------- function------------------
+    case(3)
+        do i=1,n
+           f=f+(1e6)**((i-1._dp)/(n-1._dp))*x(i)**2
+        enddo
+!--------------------------------shifted schwefels problem-------------
+    case(4)
+        do i=1,n
+            temp=0._dp
+            do j=1,i
+               temp=temp+x(j)
+            enddo
+            rnd = random_normal()
+            f = f + temp**2 * (1._dp+0.4_dp*abs(rnd))
+        enddo
+!--------------------------------rosenbrocks function------------------
+    case(6)
+        do i=1,n-1
+            f = f + 100*((x(i)+1)**2-(x(i+1)+1))**2+(x(i))**2
+        enddo
+!--------------------------------rastrigin function -------------------
+    case(9)
+        do i=1,n-1
+            f = f + (x(i)**2-10*cos(2*pi*x(i)+10)) 
+        enddo
+
+!-----------------------------------------------------------------------
+    end select
 	end subroutine fun
 !-----------------------Gaussian Adaption Algorithm-----------------------------
 	subroutine GaussAdapt(n,x,eta,f,m,C,Q,r,c_t,xmin,fmin,task)
@@ -83,8 +129,9 @@
 	real(kind=dp)                      :: wm,wc,wt,beta,p,fe,fc,dx(n)
 	real(kind=dp)                      :: fmin,xmin(n),eig_val(n),work(3*n-1)
 	real(kind=dp)                      :: detQ,dQ(n,n),test,temp(n,n),delta(n)
-	integer                            :: i,j,info
+	integer                            :: i,j,info,algorithm
 !----------------------------set GauAdapt prameter------------------------------
+    algorithm=1
 	Id=0._dp
     do i=1,n; Id(i,i)=1._dp; enddo
     wm=exp(1._dp)*n
@@ -115,8 +162,16 @@
 !-----------------------------update convariance--------------------------------
 
 !-----------------------------rank 1 update ------------------------------------
-			call DGER(n,n,1._dp/wc,delta,int(1),delta,int(1),(1._dp-1._dp/wc)*C,n)
-			dQ=C
+     select case (algorithm)
+        case(1)
+            dQ=(1._dp-1._dp/wc)*Id
+            call DSYR('l',n,1._dp/wc,eta,int(1),dQ,n)
+        case(2)
+            dQ=(1._dp-1._dp/wc)*C
+            call DSYR('l',n,1._dp/wc,delta,int(1),dQ,n)
+            C=dQ
+!			call DGER(n,n,1._dp/wc,delta,int(1),delta,int(1),(1._dp-1._dp/wc)*C,n)
+     end select
 !-----------------------------eig.val decomposition-----------------------------
 			call DSYEV( 'V', 'l', n, dQ, n, eig_val, WORK, 3*n-1, INFO )
 			if (info.ne.0) task='stop_eigendecomp'
@@ -132,7 +187,18 @@
             eig_val=eig_val/detQ
             eig_val=sqrt(eig_val)
 !-----------------------------update Q such that Q=Q*D^1/2------------------------
-            do i=1,n; Q(:,i)=dQ(:,i)*sqrt(eig_val(i)); enddo
+            do i=1,n; dQ(:,i)=dQ(:,i)*sqrt(eig_val(i)); enddo
+      select case (algorithm)
+         case(1)
+            call dgemm('n','t',n,n,n,1._dp,dQ,n,dQ,n,0._dp,temp,n)
+            dQ=temp
+            call dsymm('r','l',n,n,1._dp,dQ,n,Q,n,0._dp,temp,n)
+            Q=temp
+            call dgemm('n','t',n,n,n,1._dp,Q,n,Q,n,0._dp,temp,n)
+            C=temp
+          case(2)
+            Q=dQ
+      end select
 !--------------------------------update Q---------------------------------------
 		else
 !---------------rejected lower step size dont adopt cov. + mean-----------------
@@ -166,18 +232,3 @@
 	
 	end subroutine
 
-
-SUBROUTINE init_random_seed()
-  INTEGER :: i, n, clock
-  INTEGER, DIMENSION(:), ALLOCATABLE :: seed
-
-  CALL RANDOM_SEED(size = n)
-  ALLOCATE(seed(n))
-
-  CALL SYSTEM_CLOCK(COUNT=clock)
-
-  seed = clock + 37 * (/ (i - 1, i = 1, n) /)
-  CALL RANDOM_SEED(PUT = seed)
-
-  DEALLOCATE(seed)
-END SUBROUTINE
